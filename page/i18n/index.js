893c3c5b97dba484e3fd0d5532ab2f7aa08bab07
shoma-endo <nissin20991@gmail.com>
2025-02-17 20:12:37 +0900

    日本語ページ追加

--- page/i18n/index.js
+++ page/i18n/index.js
@@ -1,12 +1,17 @@
 // const zh = require("./zh")
 import zh from './zh'
 import en from './en'
+import ja from './ja'
 
 let i18n = en
 try {
     var res = tt.getSystemInfoSync();
-    if (res.language && res.language.indexOf('zh') != -1) {
-        i18n = zh;
+    if (res.language) {
+        if (res.language.indexOf('zh') !== -1) {
+            i18n = zh;
+        } else if (res.language.indexOf('ja') !== -1) {
+            i18n = ja;
+        }
     }
     console.log(res)
 } catch (error) {
---
+++ page/i18n/ja.js
@@ -0,0 +1,243 @@
+const ja = {
+    "button": "ボタン",
+    "component": {
+        "basic_content": "基本コンテンツ",
+        "view_container": "ビューコンテナ",
+        "form": "フォーム",
+        "navigation": "ナビゲーション",
+        "media_component": "メディアコンポーネント",
+        "other": "その他",
+        "component_tip": "以下は、ミニプログラムの公式コンポーネント機能を紹介します。コンポーネントのスタイルは参考用であり、開発者は必要に応じてカスタマイズできます。具体的なプロパティパラメータについてはドキュメントを参照してください。"
+    },
+    "button": {
+        "button": "ボタン",
+        "main_operation_normal": "メイン操作 通常",
+        "main_operation_loading": "メイン操作 読み込み中", 
+        "main_operation_disabled": "メイン操作 無効",
+        "secondary_operation_normal": "サブ操作 通常",
+        "secondary_operation_loading": "サブ操作 読み込み中",
+        "secondary_operation_disabled": "サブ操作 無効",
+        "warning_operation_normal": "警告操作 通常",
+        "warning_operation_loading": "警告操作 読み込み中",
+        "warning_operation_disabled": "警告操作 無効",
+        "disabled_button": "クリック不可のボタン"
+    },
+    "canvas": {
+        "generate_img": "画像を生成"
+    },
+    "checkbox": {
+        "default_style": "デフォルトスタイル",
+        "selected": "選択済み",
+        "unselected": "未選択",
+        "recommend_display_style": "推奨表示スタイル"
+    },
+    "form": {
+        "option_one": "オプション1",
+        "option_two": "オプション2",
+        "submit": "送信",
+        "reset": "リセット",
+        "input_placeholder": "これは入力欄です"
+    },
+    "icon": {
+        "success": "成功",
+        "success_tip": "操作が正常に完了したことを示すために使用",
+        "prompt": "プロンプト",
+        "prompt_tip": "情報提示に使用。また、操作に必要な条件が不足している場合のユーザーへの通知にも使用",
+        "normal_warning": "通常警告",
+        "normal_warning_tip": "操作後に一定の結果が生じる状況を示すために使用。また、システム要因による負の結果を示すためにも使用",
+        "strong_warning": "強い警告",
+        "strong_warning_tip": "ユーザーの操作による負の結果を示すために使用。また、操作後に重大な結果が生じる状況を示すためにも使用",
+        "wait": "待機",
+        "wait_tip": "待機状態を示し、結果を待つ必要があることをユーザーに通知するために使用",
+        "mulit_icon_selected": "複数選択アイコン_選択済み",
+        "mulit_icon_selected_tip": "複数選択コントロールで、項目が選択されていることを示すために使用", 
+        "mulit_icon_unselected": "複数選択アイコン_未選択",
+        "mulit_icon_selected_tip": "複数選択コントロールで、項目が選択可能だが未選択であることを示すために使用", 
+        "error_message": "エラーメッセージ",
+        "error_message_tip": "フォームでエラーが発生したことを示すために使用",
+        "single_icon_selected": "単一選択アイコン_選択済み",
+        "single_icon_unselected": "単一選択コントロールで、項目が選択されていることを示すために使用",
+        "download": "ダウンロード",
+        "download_tip": "ダウンロード可能であることを示すために使用",
+        "form_prompt": "プロンプト",
+        "form_prompt_tip": "フォームで情報提示があることを示すために使用",
+        "stop": "停止または閉じる",
+        "form_stop": "フォームで閉じるまたは停止を示すために使用",
+        "search": "検索",
+        "serach_tip": "検索コントロールで、検索可能であることを示すために使用"
+    },
+    "image": {
+        "local_image": "ローカル画像",
+        "net_image": "ネットワーク画像"
+    },
+    "input": {
+        "auto_focus_input": "自動フォーカス可能な入力欄",
+        "auto_focus_placeholder": "フォーカスが自動的に当たります",
+        "maxlength_input": "最大入力長を制御する入力欄",
+        "maxlength_input_placeholder": "最大入力長は10文字です",
+        "realtime_input": "リアルタイムで入力値を取得：",
+        "syncview_placeholder": "入力内容をビューに同期",
+        "control_input": "入力を制御する入力欄",
+        "control_placeholder": "連続する2つの1は2に変換されます",
+        "control_keyboard": "キーボードを制御する入力欄",
+        "control_keyboard_placeholder": "123を入力するとキーボードが閉じます",
+        "number_input": "数値入力欄",
+        "number_input_placeholder": "これは数値入力欄です",
+        "secret_input": "パスワード入力欄",
+        "secret_input_placeholder": "これはパスワード入力欄です",
+        "decimal_input": "小数点付き入力欄",
+        "decimal_input_placeholder": "小数点付き数値キーボード",
+        "id_input": "身分証明書入力欄",
+        "id_input_placeholder": "身分証明書入力キーボード",
+        "color_input": "プレースホルダーの色を制御する入力欄",
+        "color_input_placeholder": "プレースホルダーの文字色が赤色です"
+    },
+    "label": {
+        "label_title_one": "labelタグ内のフォームコンポーネント",
+        "label_title_two": "labelのfor属性でフォームコンポーネントを指定",
+        "label_title_three": "label内に複数ある場合は最初の要素を選択",
+        "option_one": "オプション1",
+        "option_two": "オプション2",
+        "default_one": "このlabel内のテキストをクリックすると、最初のチェックボックスがデフォルトで選択されます"
+    },
+    "navigator": {
+        "open_in_new_page": "新しいページで開く",
+        "open_in_current_page": "現在のページで開く"
+    },
+    "picker": {
+        "tip": "ボタンを5秒間長押しして、データの変化を確認してください",
+        "current_option": "現在の選択: ",
+        "custom_picker": "通常のピッカー",
+        "custom_object_picker": "通常のオブジェクトピッカー",
+        "multiSelector": "複数列ピッカー",
+        "time_picker": "時間ピッカー",
+        "date_picker": "日付ピッカー"
+    },
+    "radio": {
+        "default_style": "デフォルトスタイル",
+        "selected": "選択済み",
+        "unselected": "未選択",
+        "recommend_display_style": "推奨表示スタイル"
+    },
+    "richtext": {
+        "need_upload": "画像のアップロードが必要",
+        "need_pickerview": "pickerViewが必要",
+        "need_contact": "@連絡先が必要",
+        "need_emoji": "絵文字の入力",
+        "enable_return_key": "内容が空の場合に送信可能",
+        "pop_richtext": "リッチテキストボックスを表示"
+    },
+    "slider": {
+        "set_step": "ステップを設定",
+        "show_current_value": "現在の値を表示",
+        "set_range": "最小/最大値を設定"
+    },
+    "swiper": {
+        "indicator": "インジケーター",
+        "auto_play": "自動再生",
+        "vertical_direction": "垂直方向",
+        "change_duration": "スライド切り替え時間(ms)",
+        "gap_duration": "自動再生の間隔(ms)"
+    },
+    "switch": {
+        "default_style": "デフォルトスタイル",
+        "selected": "オン",
+        "unselected": "オフ",
+        "recommend_display_style": "推奨表示スタイル"
+    },
+    "textarea": {
+        "height_auto_fix_textarea": "入力エリアの高さが自動調整され、スクロールバーは表示されません",
+        "auto_focus_textare": "これは自動フォーカス可能なテキストエリアです"
+    },
+    "video": {
+        "barrage_content": "コメント内容",
+        "barrage_placeholder": "ここにコメントを入力してください",
+        "send_barrage": "コメントを送信",
+        "play": "再生",
+        "pause": "一時停止"
+    },
+    "text": {
+        "add_line": "テキストを1行追加",
+        "delete_line": "テキストを1行削除"
+    },
+    "api": {
+        "open_ability": "オープン機能",
+        "login": "ログイン",
+        "get_user_info": "ユーザー情報の取得",
+        "share": "共有",
+        "contact": "連絡先",
+        "biometric": "生体認証",
+        "password_verify": "パスワード二次認証",
+        "send_email": "メール送信",
+        "check_watermark": "ウォーターマークの確認",
+        "live_check": "生体検知",
+        "api_tip": "以下は、ミニプログラムのAPI機能のデモです。具体的なプロパティパラメータについては開発ドキュメントを参照してください。",
+        "choose_chat": "チャットを選択",
+        "device_id": "デバイスID",
+        "network": "ネットワーク",
+        "send_a_request": "リクエストを送信",
+        "webSocket": "WebSocket",
+        "upload_file": "ファイルをアップロード",
+        "download_file": "ファイルをダウンロード",
+        "media": "メディア",
+        "image": "画像",
+        "recording": "録音",
+        "file": "ファイル",
+        "audio": "音声",
+        "video": "動画",
+        "storage": "データストレージ",
+        "location": "位置情報",
+        "get_location": "現在位置を取得",
+        "open_location": "位置情報を表示",
+        "choose_location": "位置を選択",
+        "device": "デバイス",
+        "launch_query": "起動パラメータ",
+        "canvas": "キャンバスを作成",
+        "get_system_info": "システム情報を取得",
+        "get_network_type": "ネットワーク状態を取得",
+        "monitor_network_change": "ネットワーク変更を監視",
+        "monitor_gravity_sensing_data": "重力センサーデータを監視",
+        "monitor_compass_data": "コンパスデータを監視",
+        "make_phone_call": "電話をかける",
+        "scan_code": "QRコードをスキャン",
+        "clipboard": "クリップボード",
+        "screen_brightness": "画面の明るさ",
+        "vibrate": "振動",
+        "monitor_user_screen_capture": "ユーザーのスクリーンショットを監視",
+        "interface": "インターフェース",
+        "toast_tip": "トースト通知を表示",
+        "modal_tip": "モーダルダイアログを表示",
+        "actionsheet_tip": "アクションシートを表示",
+        "set_navigation_bar_title": "ナビゲーションバーのタイトルを設定",
+        "create_animation": "アニメーションを作成",
+        "page_position": "ページ位置",
+        "pull_down_refresh": "プルダウン更新",
+        "navigation": "ナビゲーション"
+    },
+    "set_tab_bar": {
+        "tab_icon": "タブアイコンを設定",
+        "remove_icon": "タブアイコンを削除",
+        "show_dot": "赤点を表示",
+        "remove_dot": "赤点を削除",
+        "custom_style": "タブスタイルをカスタマイズ（テキスト変更）",
+        "remove_custom_style": "カスタムスタイルを削除（テキスト変更）",
+        "custom_tab_info": "タブ情報をカスタマイズ",
+        "remove_tab_info": "カスタム情報を削除",
+        "hide_tab_bar": "タブバーを非表示",
+        "show_tab_bar": "タブバーを表示",
+        "back": "前のページに戻る",
+        "show_text": "テキストのみ表示",
+        "show_icon_and_selectedIcon": "アイコンと選択済みアイコンを表示",
+        "show_icon": "アイコンのみ表示",
+        "show_selectedIcon": "選択済みアイコンのみ表示"
+    },
+    "action_sheet": {
+        "pop_action_sheet": "アクションシートを表示"
+    },
+    "animation": {
+        "rotate": "回転",
+        "scale": "拡大縮小"
+    }
+}
+
+export default ja; 
